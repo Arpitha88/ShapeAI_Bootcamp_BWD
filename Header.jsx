2778775582549d450d1e351552b5b3823d516fe6
Arpitha88 <86523811+Arpitha88@users.noreply.github.com>
2021-06-26 19:24:31 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Arpitha Kidooo.com</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js BootCamp</h1>
+      <p>A Basic web dev React Js By SJP Ec BootCamp</p>
+    </div>
+  );
+}
+
+export default Info;
